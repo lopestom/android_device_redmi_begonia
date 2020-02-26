5092a7052cf0bf2ad306555e7525b8563e067d90
George Antoniadis <37894098+geoanto555@users.noreply.github.com>
2020-02-26 21:20:19 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+Device Tree for  Redmi Note 8 Pro
+
+Working :
+
+ADB
+
+Decryption userdata (pin-"pattern")
+
+Correct screenshot color
+
+MTP
+
+To compile
+
+build/envsetup.sh
+
+export ALLOW_MISSING_DEPENDENCIES=true
+
+lunch omni_begonia-eng
+
+make -jX recoveryimage
+
---
+++ omni_begonia.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2020 Geo_Project
+
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := begonia
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := begonia
+PRODUCT_NAME := omni_begonia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 8 Pro
+PRODUCT_MANUFACTURER := Xiaomi
